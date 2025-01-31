fd8e1a3e97a015c6cb1f6663bed01c85d5703e22
Chethanvr06 <chethanvr06@gmail.com>
2025-01-31 15:05:56 +0530

    Update main.tf

--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "default" {
   allocated_storage       = 10
   storage_type            = "gp2"
   engine                  = "mysql"
-  engine_version          = "5.7.34"  # A stable version of MySQL 5.7
+  engine_version          = "8.0.40"  # A stable version of MySQL 5.7
   instance_class          = "db.t3.micro"  # Change instance class to a supported one
   identifier              = var.mysql_db_identifier
   username                = var.mysql_username
