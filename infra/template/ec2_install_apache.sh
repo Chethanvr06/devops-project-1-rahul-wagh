7d98a9dbe27bdf32d55414a28810802870ec8076
Chethanvr06 <chethanvr06@gmail.com>
2025-02-03 12:19:55 +0530

    Update ec2_install_apache.sh

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -2,9 +2,9 @@
 # shellcheck disable=SC2164
 cd /home/ubuntu
 
-# Update the system and install python3, pip3
+# Update the system and install python3, pip3, and python3-venv if not installed
 yes | sudo apt update
-yes | sudo apt install python3 python3-pip
+yes | sudo apt install python3 python3-pip python3-venv
 
 # Clone the project repository
 git clone https://github.com/Chethanvr06/python-mysql-db-proj-1-rahul-wagh
@@ -15,16 +15,23 @@ sleep 20
 # Navigate to the project directory
 cd python-mysql-db-proj-1
 
-# Install required Python dependencies
-echo 'Installing required Python dependencies...'
-pip3 install --upgrade pip  # Ensure pip is up-to-date
-pip3 install -r requirements.txt  # Install project dependencies
+# Create a virtual environment in the project directory
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Upgrade pip inside the virtual environment
+pip install --upgrade pip
+
+# Install required Python dependencies inside the virtual environment
+pip install -r requirements.txt
 
 # Wait before running the application
 echo 'Waiting for 30 seconds before running the app.py'
 sleep 30
 
-# Ensure the app.py runs in the background
+# Ensure the app.py runs in the background within the virtual environment
 echo 'Starting the Flask application in the background...'
 setsid python3 -u app.py &
 
